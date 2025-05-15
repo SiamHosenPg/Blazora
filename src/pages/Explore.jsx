1fcdb85941b946cc78655d273cdcd3a164215917
Siam Hosen <siamhosenpg@gmail.com>
2025-05-15 12:46:05 +0600

    Explore page Align fix

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -6,13 +6,13 @@ const Explore = () => {
   const { postData } = useContext(PostContext); // Assuming postData is an array of posts;
   return (
     <div className="Pagearea">
-      <div className="columns-3 mt-2 md:mt-4 xl:columns-4 gap-0.5 md:gap-2 xl:gap-4 pb-20 ">
+      <div className="columns-3 mt-2 md:mt-4 xl:columns-4 gap-[2px] md:gap-2 xl:gap-4 pb-20 ">
         {postData.map((post, index) => {
           return (
             <NavLink
               to={`/post/${post.post_id}`}
               key={index}
-              className="bg-white block mb-1 md:mb-2 xl:mb-4 border border-df"
+              className="bg-white block mb-[2px] md:mb-2 xl:mb-4 border border-df"
             >
               <img src={post.media} className=" w-full h-auto " />
             </NavLink>
