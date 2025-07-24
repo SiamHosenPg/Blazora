511555e275a8ac800e1081b9f2c566d12e8f4fec
Shium Hossen <siamhosenpg@gmail.com>
2025-07-24 22:12:40 +0600

    Loading Working

--- src/components/loader/Loading.jsx
+++ src/components/loader/Loading.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useDarkMode } from "../../hook/Darkmood";
 
 const Loading = () => {
+  const [darkMode] = useDarkMode();
   return (
-    <div className="bg-background">
-      <div className="flex bg-background-secondary items-center justify-center h-screen w-full">
+    <div className={` ${darkMode ? "bg-gray-950" : "bg-gray-100"}`}>
+      <div className="flex  items-center justify-center h-screen w-full">
         <div className="w-16 h-16 border-4 border-amber-700 border-dashed rounded-full animate-spin"></div>
       </div>
     </div>
